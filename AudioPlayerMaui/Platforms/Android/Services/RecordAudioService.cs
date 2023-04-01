7c2a27aa7a6b7fa037c5fdb4aba52fc9d60eebc8
Kanat9494 <kkanat94@gmail.com>
2023-04-01 12:40:09 +0600

    1

--- AudioPlayerMaui/Platforms/Android/Services/RecordAudioService.cs
+++ AudioPlayerMaui/Platforms/Android/Services/RecordAudioService.cs
@@ -10,5 +10,66 @@ public class RecordAudioService : IRecordAudioService
 
     #endregion
 
-#
+    #region Methods
+    public void StartRecord()
+    {
+        if (mediaRecorder == null)
+        {
+            SetAudioFilePath();
+            mediaRecorder = new MediaRecorder();
+            mediaRecorder.Reset();
+            mediaRecorder.SetAudioSource(AudioSource.Mic);
+            mediaRecorder.SetOutputFormat(OutputFormat.AacAdts);
+            mediaRecorder.SetAudioEncoder(AudioEncoder.Aac);
+            mediaRecorder.SetOutputFile(storagePath);
+            mediaRecorder.Prepare();
+            mediaRecorder.Start();
+        }
+        else
+            mediaRecorder.Resume();
+
+        isRecordStarted = true;
+    }
+
+    public void PauseRecord()
+    {
+        if (mediaRecorder == null)
+            return;
+
+        mediaRecorder.Pause();
+        isRecordStarted = false;
+    }
+
+    public void ResetRecord()
+    {
+        if (mediaRecorder != null)
+        {
+            mediaRecorder.Resume();
+            mediaRecorder.Reset();
+        }
+
+        mediaRecorder = null;
+        isRecordStarted = false;
+    }
+
+    public string StopRecord()
+    {
+        if (mediaRecorder == null)
+            return string.Empty;
+
+        mediaRecorder.Resume();
+        mediaRecorder.Stop();
+        mediaRecorder = null;
+        isRecordStarted = false;
+        return storagePath;
+    }
+
+    private void SetAudioFilePaht()
+    {
+        string fileName = "/Record_" + DateTime.UtcNow.ToString("ddMMM_hhmmss") + ".mp3";
+        var path = Environment.GetFolderPath(System.Environment.SpecialFolder.MyDocuments);
+        storagePath = path + fileName;
+        Directory.CreateDirectory(path);
+    }
+    #endregion
 }
